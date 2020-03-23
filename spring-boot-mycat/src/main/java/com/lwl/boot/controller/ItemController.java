7e22384e53ec02ab86ab7ad33a28dd8542ce1e95
ChenXb <810464826@qq.com>
2020-03-23 20:32:26 +0800

    Update ItemController.java

--- spring-boot-mycat/src/main/java/com/lwl/boot/controller/ItemController.java
+++ spring-boot-mycat/src/main/java/com/lwl/boot/controller/ItemController.java
@@ -20,7 +20,7 @@ public class ItemController {
 	private ItemService itemService;
 	
 	@RequestMapping("/add")
-	public String add(Long id,String value) {
+	public String add(Long id,Integer value) {
 		Item i = new Item();
 		i.setId(id).setValue(value).setIndate(new Date());
 		itemService.add(i);
